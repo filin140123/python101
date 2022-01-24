e5ed7b0f3bf086798ac658089ca50c2eece92a53
filin140123 <37931708+filin140123@users.noreply.github.com>
2022-01-24 08:15:54 +0500

    Update task3-dict.py
    новое решение для task3-dict

--- hw02/task3-dict.py
+++ hw02/task3-dict.py
@@ -1,19 +1,15 @@
 seasons = {
-    "winter": [1, 2, 12],
-    "spring": [3, 4, 5],
-    "summer": [6, 7, 8],
-    "autumn": [9, 10, 11],
+    "Зима": [1, 2, 12],
+    "Весна": [3, 4, 5],
+    "Лето": [6, 7, 8],
+    "Осень": [9, 10, 11],
 }
 
 month = int(input("Введите номер месяца: "))
 
-if month in seasons["winter"]:
-    print("Зима")
-elif month in seasons["spring"]:
-    print("Весна")
-elif month in seasons["summer"]:
-    print("Лето")
-elif month in seasons["autumn"]:
-    print("Осень")
+for k, v in seasons.items():
+    if month in v:
+        print(k)
+        break
 else:
     print("Нет такого месяца")
