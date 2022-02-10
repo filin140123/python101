05d30abb77cd9625bbaf6d59da964271e5684376
filin140123 <37931708+filin140123@users.noreply.github.com>
2022-02-10 22:35:11 +0500

    Update task3.py

--- hw07/task3.py
+++ hw07/task3.py
@@ -3,20 +3,19 @@ class Cell:
         self.number = number
 
     def __add__(self, other):
-        return self.number + other.number
+        return Cell(self.number + other.number)
 
     def __sub__(self, other):
-        result = self.number - other.number
-        if result > 0:
-            return result
+        if self.number - other.number > 0:
+            return Cell(self.number - other.number)
         else:
             raise ValueError("Разность количества ячеек двух клеток меньше нуля")
 
     def __mul__(self, other):
-        return self.number * other.number
+        return Cell(self.number * other.number)
 
     def __truediv__(self, other):
-        return self.number // other.number
+        return Cell(self.number // other.number)
 
     def make_order(self, row_size):
         s, e = divmod(self.number, row_size)
@@ -32,3 +31,6 @@ print(c1.make_order(5))
 c2 = Cell(42)
 print(c2.make_order(10))
 print(c2.make_order(7))
+
+c3 = c2 + c1
+print(c3.make_order(8))
