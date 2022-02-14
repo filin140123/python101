55f61c9cbe3e30ccf775764ad7ed90982cc27852
filin140123 <37931708+filin140123@users.noreply.github.com>
2022-02-14 18:19:22 +0500

    Delete hw05 directory

--- hw05/task5.py
+++
@@ -1,6 +0,0 @@
-with open("task5-file.txt", "w", encoding="utf-8") as f:
-    f.write("14 22 31 48 56 65 75 84 99")
-
-with open("task5-file.txt", "r", encoding="utf-8") as r:
-    numbers = sum([int(i) for i in r.read().split(" ")])
-    print(numbers)
